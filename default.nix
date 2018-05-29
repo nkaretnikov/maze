71fdef9e4474e9123f295e9c4fe9d0f02526bd28
Nikita Karetnikov <nikita@karetnikov.org>
2018-05-29 22:43:47 +0300

    Switch to a newer reflex-dom-canvas version

--- default.nix
+++ default.nix
@@ -1,17 +1,12 @@
 (import ./reflex-platform {}).project ({ pkgs, ... }: {
-  # Disable Haddock since it breaks the build.
-  overrides = self: super: {
-    reflex-dom-canvas = pkgs.haskell.lib.dontHaddock super.reflex-dom-canvas;
-  };
-
   packages = {
     maze = ./.;
     # nix-prefetch-url --unpack https://github.com/OWNER/REPO/archive/REV.tar.gz
     reflex-dom-canvas = pkgs.fetchFromGitHub {
       owner = "qfpl";
       repo = "reflex-dom-canvas";
-      rev = "2e640ae5b759ea8732d098a6ef4bec701dc42bda";
-      sha256 = "0a8fpjzpncfsbaqjgw6wgg4saskqc8x71kc1sp1m7f46l5fw11qa";
+      rev = "fcabeaf63c60753e0dbb07aab93bc1d2b11db9b8";
+      sha256 = "05b0s1k1a144jyl0zg8crpiqwfl710ncdzyswm4cszrsdfz00rc0";
     };
   };
 
